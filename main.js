1c5a36f181271d3236a3cb9f103de43e8dd00b24
BohdanPotapchuk <101199283+BohdanPotapchuk@users.noreply.github.com>
2022-04-18 19:33:58 +0300

    Update main.js
    deleted two variable

--- main.js
+++ main.js
@@ -5,9 +5,7 @@ btn = document.querySelector('button');
 var overlay = document.querySelector('.overlay');
 
 /* Looping through images */
-var b = ['1','2','3','4','5']
 var selectedPic 
-var a = ['images/pic1.jpg','images/pic2.jpg','images/pic3.jpg','images/pic4.jpg','images/pic5.jpg'] ;
 for(a = 1 ; a < 6 ; a++) {
 
 };
