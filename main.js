69ffc995d75d4f3777789e1bd0da971988a21dbb
BohdanPotapchuk <baaaagdan4444@gmail.com>
2022-04-20 20:13:37 +0300

    finished image looping

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
+'use strict'
 var displayedImage = document.querySelector('.displayed-img');
 var thumbBar = document.querySelector('.thumb-bar');
 
-btn = document.querySelector('button');
+var btn = document.querySelector('button');
 var overlay = document.querySelector('.overlay');
 
 /* Looping through images */
-var selectedPic 
-for(a = 1 ; a < 6 ; a++) {
 
+for(var selectedPic = 1 ; selectedPic < 6 ; selectedPic++) {
+  createNewPic(selectedPic)
 };
 
-function createNewPic (selectedPic) {
+function createNewPic (indexPic) {
   var newImage = document.createElement('img');
-  newImage.setAttribute('src', `images/pic${selectedPic}.jpg`);
+  newImage.setAttribute('src', `images/pic${indexPic}.jpg`);
   thumbBar.appendChild(newImage);
 }
 /* Wiring up the Darken/Lighten button */
