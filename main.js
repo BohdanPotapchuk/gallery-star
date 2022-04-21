c8b78e9bd8206a02d75252e359c9d6798b2a4abc
BohdanPotapchuk <baaaagdan4444@gmail.com>
2022-04-21 22:29:58 +0300

    added click on thumbar

--- main.js
+++ main.js
@@ -6,15 +6,19 @@ var btn = document.querySelector('button');
 var overlay = document.querySelector('.overlay');
 
 /* Looping through images */
+var src = displayedImage.getAttribute('src');
+thumbBar.addEventListener('click', function(){
+  var getTargetSrc = event.target.getAttribute('src')
+  displayedImage.setAttribute('src',`${getTargetSrc}`)
+})
 
 for(var selectedPic = 1 ; selectedPic < 6 ; selectedPic++) {
-  createNewPic(selectedPic)
+  createNewPic(selectedPic);
 };
-
 function createNewPic (indexPic) {
   var newImage = document.createElement('img');
   newImage.setAttribute('src', `images/pic${indexPic}.jpg`);
   thumbBar.appendChild(newImage);
-}
+};
 /* Wiring up the Darken/Lighten button */
 
