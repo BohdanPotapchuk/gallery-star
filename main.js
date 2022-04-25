362b30b4562e00295dcbbd1b3ac072e16bed56ef
BohdanPotapchuk <baaaagdan4444@gmail.com>
2022-04-25 20:04:55 +0300

    added html&css element slider

--- main.js
+++ main.js
@@ -1,24 +1,48 @@
 'use strict'
-var displayedImage = document.querySelector('.displayed-img');
-var thumbBar = document.querySelector('.thumb-bar');
+let displayedImage = document.querySelector('.displayed-img');
+let thumbBar = document.querySelector('.thumb-bar');
 
-var btn = document.querySelector('button');
-var overlay = document.querySelector('.overlay');
+let btn = document.querySelector('button');
+let overlay = document.querySelector('.overlay');
+
+let arrowLeft = document.querySelector('.arrow_left');
+let arrowRight = document.querySelector('.arrow_right');
 
 /* Looping through images */
-var src = displayedImage.getAttribute('src');
 thumbBar.addEventListener('click', function(){
-  var getTargetSrc = event.target.getAttribute('src')
-  displayedImage.setAttribute('src',`${getTargetSrc}`)
-})
+  let getTargetSrc = event.target.getAttribute('src');
+  displayedImage.setAttribute('src',`${getTargetSrc}`);
+});
 
-for(var selectedPic = 1 ; selectedPic < 6 ; selectedPic++) {
+for(let selectedPic = 1 ; selectedPic < 6 ; selectedPic++) {
   createNewPic(selectedPic);
-};
+}
 function createNewPic (indexPic) {
-  var newImage = document.createElement('img');
+  let newImage = document.createElement('img');
   newImage.setAttribute('src', `images/pic${indexPic}.jpg`);
   thumbBar.appendChild(newImage);
-};
+}
 /* Wiring up the Darken/Lighten button */
 
+let btnClass = btn.getAttribute('class');
+btn.addEventListener('click', function(){
+  if (btn.className === 'dark') {
+    overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+    btn.setAttribute('class','light');
+    btn.textContent = 'Светлее';
+  } else if (btn.className === 'light') {
+    overlay.style.backgroundColor = "rgba(0,0,0,0.0)";
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Темнее';
+  }
+});
+
+/* Made sliders */ 
+let currentImg = displayedImage.getAttribute('src');
+
+arrowLeft.addEventListener('click', function(){
+ 
+});
+arrowRight.addEventListener('click', function(){
+  
+});
